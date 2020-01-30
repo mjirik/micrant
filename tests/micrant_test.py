9ad6b1556e10a37dfe53c70f2cd736bc5d894009
Miroslav Jirik <miroslav.jirik@gmail.com>
2020-01-30 18:11:17 +0100

    more tests for setting value to left

--- tests/micrant_test.py
+++ tests/micrant_test.py
@@ -81,7 +81,7 @@ def test_just_create_next_image():
     mapp.gui_left_is_lower_and_show_next()
     assert len(mapp.report.df) == 0
 
-def test_annotation_left_right_and_set():
+def test_annotation_left_right_and_write_data():
     qapp = global_qapp
     # qapp = QtWidgets.QApplication(sys.argv)
     mapp = micrant.micrant_app.MicrAnt()
@@ -97,12 +97,41 @@ def test_annotation_left_right_and_set():
     mapp.gui_right_is_lower_and_show_next()
     assert len(mapp.report.df) == 2
 
+    df1 = pd.read_excel(TEST_XLSX)
+    mapp.save_data()
+    df2 = pd.read_excel(TEST_XLSX)
+    assert len(df1) < len(df2)
+
+
+def test_annotation_set_value():
+    qapp = global_qapp
+    # qapp = QtWidgets.QApplication(sys.argv)
+    mapp = micrant.micrant_app.MicrAnt()
+    mapp.set_common_spreadsheet_file(TEST_XLSX)
+    mapp.start_gui(skip_exec=True, qapp=qapp)
+    mapp.set_parameter("Annotation;Annotated Parameter", "SNI")
+    mapp.set_parameter("Annotation;Lower Threshold", 0)
+    mapp.set_parameter("Annotation;Upper Threshold", 2)
+    logger.debug("before gui_next_image()")
+    mapp.gui_left_is_lower_and_show_next()
+    assert len(mapp.report.df) == 0
+
     lrow = mapp._row1
+    expected_n_rows = 1
     mapp.gui_set_left_75_percent_and_show_next()
-    assert len(mapp.report.df) == 3
-    assert mapp.report.df["SNI"].iat[2] == 1.5
-    assert lrow["Annotation ID"] == mapp.report.df["Annotation ID"].iat[2]
-    assert lrow["File Name"] == mapp.report.df["File Name"].iat[2]
+    assert len(mapp.report.df) == expected_n_rows
+    assert mapp.report.df["SNI"].iat[expected_n_rows - 1] == 1.5
+    assert lrow["Annotation ID"] == mapp.report.df["Annotation ID"].iat[expected_n_rows - 1]
+    assert lrow["File Name"] == mapp.report.df["File Name"].iat[expected_n_rows - 1]
+
+    lrow = mapp._row1
+    expected_n_rows = 2
+    mapp.set_parameter("Left Image Annotation;New Parameter Value", 1.2)
+    mapp.gui_set_left_image_and_show_next()
+    assert len(mapp.report.df) == 2
+    assert mapp.report.df["SNI"].iat[expected_n_rows - 1] == 1.2
+    assert lrow["Annotation ID"] == mapp.report.df["Annotation ID"].iat[expected_n_rows - 1]
+    assert lrow["File Name"] == mapp.report.df["File Name"].iat[expected_n_rows - 1]
 
 
     df1 = pd.read_excel(TEST_XLSX)
